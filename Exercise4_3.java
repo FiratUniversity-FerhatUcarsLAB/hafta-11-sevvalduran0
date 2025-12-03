864a9bdab07b17707bb11cc695a00c546783ffed
sevvalduran0 <240541088@firat.edu.tr>
2025-12-03 21:25:13 +0300

    Update Exercise4_3.java

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -22,3 +22,59 @@ public class Exercise4_3 {
         System.out.println(".");
     }
 }
+
+/*
+----------------------------------------------------------
+1) STACK DIYAGRAMI (ping ilk kez çağrıldığında)
+----------------------------------------------------------
+
+main(args)
+   |
+   --> zoop()
+           |
+           --> baffle()
+                    |
+                    --> ping()   <-- ilk ping çağrısı
+
+Stack görünümü:
+
++---------------------------+
+| ping()                    |
+| (parametre yok)           |
++---------------------------+
+| baffle()                  |
+| (parametre yok)           |
++---------------------------+
+| zoop()                    |
+| (parametre yok)           |
++---------------------------+
+| main(String[] args)       |
++---------------------------+
+
+
+
+----------------------------------------------------------
+2) PROGRAM ÇIKTISI (birebir, boşluk ve satır sonlarıyla)
+----------------------------------------------------------
+
+No, I wug.
+You wugga wug.
+I wug.
+
+Açıklama:
+
+main -> "No, I "
+zoop -> baffle -> "wug."
+     -> "You wugga "
+     -> baffle -> "wug."
+main -> "I "
+main -> baffle -> "wug."
+
+Hepsi birleşince:
+
+No, I wug.
+You wugga wug.
+I wug.
+
+----------------------------------------------------------
+*/
